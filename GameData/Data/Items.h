61a23e6999ee979c928bf814315af314a62a9437
modernmenace <lukebortz1>
2020-02-15 13:50:23 -0500

    Inventory Work

--- GameData/Data/Items.h
+++ GameData/Data/Items.h
@@ -2,7 +2,6 @@
 #define ENFUSION___ITEMS_H
 
 #include "../Engine/Misc/Item.h"
-#include "ItemEffects.h"
 #include "../Engine/Misc/ItemRegistry.h"
 
 CREATE_ITEM(1, IT_1, "tomato", "A tasty fruit. Or is it a vegetable? A tasty vegetable.",
