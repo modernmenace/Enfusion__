8362e64e465098e8f390d5c8d2660fd6bf370ac4
modernmenace <lukebortz1>
2020-04-16 16:09:45 -0400

    Inventory work

--- GameData/Engine/Entity/UI/Tooltip.cpp
+++ GameData/Engine/Entity/UI/Tooltip.cpp
@@ -1,6 +1,7 @@
 #include "Tooltip.h"
 
-Tooltip::Tooltip()
+Tooltip::Tooltip() : t_iName("", sf::Vector2f(0, 0), 20),
+                     t_iDesc("", sf::Vector2f(0, 0), 20)
 {
     addComponent<Position>(sf::Vector2f(0, -0));
     addComponent<Sprite>("UI/windowsheet.png");
@@ -11,7 +12,9 @@ void Tooltip::initialize()
     dbg_log("init");
     getComponent<Sprite>().getSprite().setTextureRect(sf::IntRect(0, 0, 48, 48));
     getComponent<Sprite>().getSprite().setScale(4, 4);
-    getComponent<Sprite>().visible = false;
+
+    t_iName.initialize();
+    t_iDesc.initialize();
 }
 
 void Tooltip::show(Item* item, sf::Vector2f position)
@@ -20,6 +23,24 @@ void Tooltip::show(Item* item, sf::Vector2f position)
     getComponent<Sprite>().getSprite().setPosition(position);
     getComponent<Sprite>().visible = true;
 
-    //TODO: show item text
-    
+    t_iName.setText(item->name());
+    t_iDesc.setText(item->description());
+
+    sf::Vector2f pos = getComponent<Position>().getPosition();
+    pos.x += 15;
+    pos.y += 10;
+    t_iName.setPosition(pos);
+    pos.y += 20;
+    t_iDesc.setPosition(pos);
+
+    t_visible = true;
+}
+
+void Tooltip::render(sf::RenderWindow* window)
+{
+    if (!t_visible) return;
+    Entity::render(window);
+
+    t_iName.render(window);
+    t_iDesc.render(window);
 }
--- GameData/Engine/Entity/UI/Tooltip.h
+++ GameData/Engine/Entity/UI/Tooltip.h
@@ -5,14 +5,22 @@
 #include "../../Component/Base/Position.h"
 #include "../../Component/Base/Sprite.h"
 #include "../../Misc/Item.h"
+#include "TextDisplay.h"
 
 class Tooltip : public Entity {
 
 public:
     Tooltip();
-    void initialize() override;
+    void initialize()              override;
+    void render(sf::RenderWindow*) override;
     void show(Item* item, sf::Vector2f position);
-    inline void hide() { getComponent<Sprite>().visible = false; }
+    inline void hide() { t_visible = false; };
+
+private:
+    TextDisplay t_iName;
+    TextDisplay t_iDesc;
+
+    bool t_visible = false;
 
 };
 
