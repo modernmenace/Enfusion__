34c6882847a23fa4dae38fae389ff61eba66561f
Jared <jaredbortz@gmail.com>
2020-04-23 19:18:32 -0400

    Level Work

--- GameData/Engine/Lvl/MapGenerator.cpp
+++ GameData/Engine/Lvl/MapGenerator.cpp
@@ -18,11 +18,14 @@ MapGenerator* MapGenerator::Instance()
 std::vector<int> MapGenerator::generateMap(uint16_t sizeX, uint16_t sizeY)
 {
     //TODO: Generate Map Array Here
+    //TODO: Determine biome sizes and boundaries? mathematical nightmare
+    //TODO: Pass to biome classes once determined, have them return int array?
+    //TODO: Feeling originally rather than an int, should be custom type
     lvl.clear();
     m_size.x = sizeX;
     m_size.y = sizeY;
     for (int i = 0; i < (sizeX * sizeY); i++)
-        lvl.push_back(70);
+        lvl.push_back(69);
 
 
     return lvl;
