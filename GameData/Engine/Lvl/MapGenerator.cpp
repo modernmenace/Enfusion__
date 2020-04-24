281430c82ce9c8331ce0ff8d9931d79c9a28a57a
Jared <jaredbortz@gmail.com>
2020-04-23 21:19:35 -0400

    Level Work

--- GameData/Engine/Lvl/MapGenerator.cpp
+++ GameData/Engine/Lvl/MapGenerator.cpp
@@ -14,21 +14,27 @@ MapGenerator* MapGenerator::Instance()
     return m_Instance;
 }
 
-
 std::vector<int> MapGenerator::generateMap(uint16_t sizeX, uint16_t sizeY)
 {
     //TODO: Generate Map Array Here
     //TODO: Determine biome sizes and boundaries? mathematical nightmare
     //TODO: Pass to biome classes once determined, have them return int array?
     //TODO: Feeling originally rather than an int, should be custom type
-    lvl.clear();
+    m_lvl.clear();
+    m_tilemap.clear();
     m_size.x = sizeX;
     m_size.y = sizeY;
     for (int i = 0; i < (sizeX * sizeY); i++)
-        lvl.push_back(69);
+    {
+        Tile t;
+        t.tilesetID = 69;
+        t.biome     = LEVEL_BIOME_ID_WOODLAND;
+        m_lvl.push_back(t);
+        m_tilemap.push_back(t.tilesetID);
+    }
 
 
-    return lvl;
+    return tilemap();
 }
 
 
--- GameData/Engine/Lvl/MapGenerator.h
+++ GameData/Engine/Lvl/MapGenerator.h
@@ -2,21 +2,29 @@
 #define ENFUSION___MAPGENERATOR_H
 
 #include "../Core/Engine.h"
+#include "BiomeManager.h"
+
+struct Tile
+{
+    uint16_t biome;
+    uint16_t tilesetID;
+};
 
 class MapGenerator {
 
 public:
+    ~MapGenerator();
     static MapGenerator* Instance();
     std::vector<int> generateMap(uint16_t sizeX, uint16_t sizeY);
-    inline sf::Vector2i size() { return m_size; }
 
+    inline sf::Vector2i size()        { return m_size; }
+    inline std::vector<int> tilemap() { return m_tilemap; }
 
 private:
     static MapGenerator* m_Instance;
-    std::vector<int> lvl;
-
-    sf::Vector2i m_size;
-
+    std::vector<Tile>    m_lvl;
+    std::vector<int>     m_tilemap;
+    sf::Vector2i         m_size;
 
 };
 
