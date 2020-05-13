820353cbd40439ef7a15900b7407995faf247b33
modernmenace <lukebortz1>
2020-05-13 15:01:25 -0400

    Script Engine Placeholder

---
+++ GameData/Engine/ScriptEngine/ScriptEngine.h
@@ -0,0 +1,18 @@
+#ifndef ENFUSION___SCRIPTENGINE_H
+#define ENFUSION___SCRIPTENGINE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif //ENFUSION___SCRIPTENGINE_H
