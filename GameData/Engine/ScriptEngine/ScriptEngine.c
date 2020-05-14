bb6a76b283af161e92ad7105b4a983888915bd14
modernmenace <lukebortz1>
2020-05-14 12:56:45 -0400

    Script Engine Work

--- GameData/Engine/ScriptEngine/ScriptEngine.c
+++ GameData/Engine/ScriptEngine/ScriptEngine.c
@@ -1,5 +1,7 @@
 #include "python3.8/Python.h"
 #include "ScriptEngine.h"
+#include <dirent.h>
+#include <unistd.h>
 
 bool SE_init(const char* programName)
 {
@@ -20,7 +22,25 @@ bool SE_init(const char* programName)
 
 bool SE_checkForScripts(void)
 {
-    //TODO: Implement this
-    //TODO: check mod directory
+    struct dirent *de;
+    char dir[PATH_MAX];
+    getcwd(dir, sizeof(dir));
+    strncat(dir, "/Mods", 5);
+    DIR *dr = opendir(dir);
+
+    if (dr == NULL)
+    {
+        //TODO: mod directory does not exist
+        printf("Could not open current directory" );
+        return 0;
+    }
+
+    while ((de = readdir(dr)) != NULL)
+        printf("%s\n", de->d_name);
+
+    closedir(dr);
+
     return true;
+
+
 }
