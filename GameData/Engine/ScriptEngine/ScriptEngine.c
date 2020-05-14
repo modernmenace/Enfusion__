7f8f2c2f8168d5f2220f8ee9a7c230fb97f21f4e
modernmenace <lukebortz1>
2020-05-14 10:06:44 -0400

    Script Engine Work

---
+++ GameData/Engine/ScriptEngine/ScriptEngine.c
@@ -0,0 +1,7 @@
+#include <Python/Python.h>
+#include "ScriptEngine.h"
+
+void SE_init(void)
+{
+    //initialize engine here
+}
--- GameData/Engine/ScriptEngine/ScriptEngine.h
+++ GameData/Engine/ScriptEngine/ScriptEngine.h
@@ -1,18 +1,7 @@
 #ifndef ENFUSION___SCRIPTENGINE_H
 #define ENFUSION___SCRIPTENGINE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-
-
-
-
-#ifdef __cplusplus
-};
-#endif
+void SE_init(void);
 
 
 #endif //ENFUSION___SCRIPTENGINE_H
