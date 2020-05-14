f779e5202b43f5301fa24ab015369b7d205f9a12
modernmenace <lukebortz1>
2020-05-14 13:37:39 -0400

    Script Engine Work

--- GameData/Engine/ScriptEngine/ScriptEngine.c
+++ GameData/Engine/ScriptEngine/ScriptEngine.c
@@ -1,6 +1,5 @@
 #include "python3.8/Python.h"
 #include "ScriptEngine.h"
-#include <dirent.h>
 #include <unistd.h>
 
 bool SE_init(const char* programName)
@@ -30,15 +29,41 @@ bool SE_checkForScripts(void)
 
     if (dr == NULL)
     {
-        //TODO: mod directory does not exist
-        printf("Could not open current directory" );
-        return 0;
+        return false; // mod directory doesnt exist
     }
 
+    uint16_t dirCount = 0;
     while ((de = readdir(dr)) != NULL)
-        printf("%s\n", de->d_name);
+    {
+        if (strcmp(".", de->d_name) == 0 || strcmp("..", de->d_name) == 0 || strcmp(".DS_Store", de->d_name) == 0)
+            continue;
+        dirCount++;
+    }
+    rewinddir(dr);
+
+    char* dirNames[dirCount];
+    uint16_t i = 0;
+    while ((de = readdir(dr)) != NULL)
+    {
+        if (strcmp(".", de->d_name) == 0 || strcmp("..", de->d_name) == 0 || strcmp(".DS_Store", de->d_name) == 0)
+            continue;
+
+        dirNames[i] = (char*) malloc (strlen(de->d_name)+1);
+        strncpy (dirNames[i],de->d_name, strlen(de->d_name));
+        i++;
+    }
+
+    int j = 0;
+    for(j = 0; j < dirCount; j++)
+    {
+        printf(dirNames[j]);
+        printf("\n");
+    }
 
     closedir(dr);
+
+    //TODO: check each mod directory to see if mod.py is present
+
 
     return true;
 
--- GameData/Engine/ScriptEngine/ScriptEngine.h
+++ GameData/Engine/ScriptEngine/ScriptEngine.h
@@ -2,6 +2,14 @@
 #define ENFUSION___SCRIPTENGINE_H
 
 #include <stdbool.h>
+#include <dirent.h>
+
+typedef struct
+{
+    uint16_t       id;
+    char*          name;
+    char*          directory;
+} SE_Mod;
 
 bool SE_init(const char*);
 bool SE_checkForScripts(void);
