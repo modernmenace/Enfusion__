ea0539987d41b4e96b4e8497d2380fb70e377772
Jared <jaredbortz@gmail.com>
2021-08-07 21:54:39 -0400

    Z-ordering work

--- GameData/Engine/Component/Base/Z.cpp
+++ GameData/Engine/Component/Base/Z.cpp
@@ -12,11 +12,13 @@ void Z::setZ(uint8_t val)
     LevelManager::Instance()->getCurrentLevel().sortZOrder();
 }
 
-bool Z::compare(const Entity *left, const Entity *right)
+bool Z::compare(const Entity* left, const Entity* right)
 {
     //TODO: this assertion is failing... why???
     //TODO: how many times is sortZInternal being called?
     //TODO: possible it is being overloaded with several calls per update?
+    //TODO: debug and pay attention and see if you notice ocmmonalities
+    //TODO: like left always being the NULL one
     assert(left != nullptr);
     assert(right != nullptr);
 
